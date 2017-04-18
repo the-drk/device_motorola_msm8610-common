3d9844a698e4a5a22b1227f0443f19877dd905e8
Gabriele M <moto.falcon.git@gmail.com>
2017-04-18 21:45:30 +0200

    msm8610-common: power: Minor code optimization
    We can drop the unnecessarily big buffer, the unneeded sprintf and
    simply write two bytes.
    
    Change-Id: I26f075e5e5d2c954d8c4a07d2a6ccea9a656e1b7

--- power/power.c
+++ power/power.c
@@ -170,9 +170,6 @@ static void set_power_profile(int profile)
 static void power_hint(__attribute__((unused)) struct power_module *module,
                        power_hint_t hint, void *data)
 {
-    char buf[80];
-    int len;
-
     switch (hint) {
     case POWER_HINT_INTERACTION:
         if (!is_profile_valid(current_power_profile)) {
@@ -187,11 +184,9 @@ static void power_hint(__attribute__((unused)) struct power_module *module,
         if (!check_governor()) return;
 
         if (boostpulse_open() >= 0) {
-            snprintf(buf, sizeof(buf), "%d", 1);
-            len = write(boostpulse_fd, &buf, sizeof(buf));
+            int len = write(boostpulse_fd, "1", 2);
             if (len < 0) {
-                strerror_r(errno, buf, sizeof(buf));
-                ALOGE("Error writing to boostpulse: %s\n", buf);
+                ALOGE("Error writing to boostpulse: %s\n", strerror(errno));
 
                 pthread_mutex_lock(&lock);
                 close(boostpulse_fd);
