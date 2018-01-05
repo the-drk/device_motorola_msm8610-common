1e905ffc530ea535794394db27884a3cf3d57089
Andersonmendes2016 <lge500meu@gmail.com>
2018-01-05 12:59:54 -0200

    msm8610-common: Increase Performance
    - Drop Zram (moar cpu & battery)
    - Use Custom Dalvik & HWUI props
    - Increase LMK

--- msm8610.mk
+++ msm8610.mk
@@ -19,6 +19,29 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # common vendor blobs
 $(call inherit-product, vendor/motorola/msm8610-common/msm8610-common-vendor.mk)
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=128m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
+
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.mmi.boot.sh \
@@ -253,8 +276,5 @@ PRODUCT_PACKAGES += \
     wificond
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
-
-# Inhert dalvik heap values
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 $(call inherit-product-if-exists, hardware/qcom/msm8x74/msm8x74.mk)
